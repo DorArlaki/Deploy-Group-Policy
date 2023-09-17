f5ef63f6788bd0c2f31d1e3087c7062e846bf276
A. Dor <107101354+DorArlaki@users.noreply.github.com>
2023-09-17 13:52:41 +0300

    Create DC-install-tightvnc.ps1

---
+++ Remote-TightVNC-Installer/DC-install-tightvnc.ps1
@@ -0,0 +1,11 @@
+# Basic silent installation
+msiexec /i "C:\Path\Share_DC_Folder\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart
+
+# Install only the server part
+msiexec /i "C:\Path\Share_DC_Folder\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart ADDLOCAL=Server
+
+# Enable VNC authentication with a password
+msiexec /i "C:\Path\Share_DC_Folder\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=your_password
+
+# Enable control interface authentication with a password
+msiexec /i "C:\Path\Share_DC_Folder\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=your_password
