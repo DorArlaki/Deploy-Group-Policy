0fba5e0474f40567038f7cde52c5225054f137ca
A. Dor <107101354+DorArlaki@users.noreply.github.com>
2023-09-25 23:19:44 +0300

    Update TightVNC_Installation_DC_Script.ps1

--- Remote-TightVNC-Installer/TightVNC_Installation_DC_Script.ps1
+++ Remote-TightVNC-Installer/TightVNC_Installation_DC_Script.ps1
@@ -5,7 +5,7 @@ msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /no
 msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart ADDLOCAL=Server
 
 # Enable VNC authentication with a password
-msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD="your_password_here"
+msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=your_password_here
 
 # Enable control interface authentication with a password
-msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD="your_password_here"
+msiexec /i "C:\Share\GPO\Scripts\tightvnc-2.8.81-gpl-setup-64bit.msi" /quiet /norestart SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=your_password_here
